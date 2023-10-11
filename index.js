6ffeb1e36e0f5836709400ee663044c00bcaf358
Zhehao <x97589125@gmail.com>
2023-10-11 16:15:28 -0700

    add path

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 import express from "express";
 import apiRouter from "./routes/api.js";
 import bodyParser from "body-parser";
-
+import path from "path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 app.use(bodyParser.json());
-app.use(express.static("frontend"));
+app.use(express.static(path.join(__dirname, "frontend")));
 
 
 app.use(apiRouter);
